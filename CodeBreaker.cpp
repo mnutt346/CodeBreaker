6ada7011c9c7f4c3764e5f5e20b6428e039c2072
Michael Nutt <mnutt346@gmail.com>
2019-08-07 18:07:40 -0500

    Initial intro to the game prints for the user.

--- CodeBreaker.cpp
+++ CodeBreaker.cpp
@@ -1,4 +1,29 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main()
 {
+    // Print intro for user.
+    cout << "You have been locked in a room with an electronically-controlled lock..." << endl;
+    cout << "You must enter the correct code to continue..." << endl;
+
+    int Num1 = 4;
+    int Num2 = 6;
+    int Num3 = 1;
+
+    int CodeSum = Num1 + Num2 + Num3;
+    int CodeProduct = Num1 * Num2 * Num3;
+
+    // Print CodeSum and CodeProduct for the user.
+    cout << endl; 
+    cout << "+ There are 3 numbers in the code." << endl;
+    cout << "+ The code adds up to: " << CodeSum << endl;
+    cout << "+ The code multiplies to give: " << CodeProduct << endl;
+
+    int PlayerGuess;
+
     return 0;
 }
