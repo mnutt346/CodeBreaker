243a86f0e90aba3a6225c5bd5af5e114d7d3a66d
Michael Nutt <mnutt346@gmail.com>
2019-08-07 18:59:57 -0500

    User can play game with randomized codes. Level will increase for each successful attempt up to level 11.

--- CodeBreaker.cpp
+++ CodeBreaker.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
+#include <ctime>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-void PrintIntro()
+void PrintIntro(int Difficulty)
 {
     // Print intro for user.
-    cout << "You have been locked in a room with an electronically-controlled lock...\n";
+    cout << "\n\nYou have been locked in a room with a Level " << Difficulty << " electronically-controlled lock...\n";
     cout << "You must enter the correct code to continue...\n\n";
 }
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
-    PrintIntro();
+    PrintIntro(Difficulty);
 
-    int Num1 = 4;
-    int Num2 = 6;
-    int Num3 = 1;
+    int Num1 = rand() % (2 * Difficulty) + 1;
+    int Num2 = rand() % (2 * Difficulty) + 1;
+    int Num3 = rand() % (2 * Difficulty) + 1;
 
     int CodeSum = Num1 + Num2 + Num3;
     int CodeProduct = Num1 * Num2 * Num3;
 
     // Print CodeSum and CodeProduct for the user.
-    cout << "+ There are 3 numbers in the code." << endl;
+    cout << "+ There are 3 numbers in the code and each number can be 1 through 22." << endl;
     cout << "+ The code adds up to: " << CodeSum << endl;
     cout << "+ The code multiplies to give: " << CodeProduct << endl;
 
@@ -38,16 +39,37 @@ void PlayGame()
     // Check if player's guess is correct or not.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        cout << "You Win!\n" ;
+        cout << "You solved the code!\n" << "The next room will be more difficult!\n" ;
+        return true;
     }
     else
     {
-        cout << "Nope!\n";
+        cout << "Incorrect code. Try again\n";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
-    return 0;
+    srand(time(NULL)); // create new random sequence based on time of day
+
+    int LevelDifficulty = 1;
+    const int MAXDIFFICULTY = 11; // Because Spinal Tap
+
+    while(LevelDifficulty <= MAXDIFFICULTY)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        cin.clear(); // Clears input errors
+        cin.ignore(); // Removes buffer
+
+        // If player completes the level:
+        if (bLevelComplete)
+        {
+            // Increase level difficulty
+            LevelDifficulty++;
+        }
+    }    
+
+    cout << "\n***** Well done! You have escaped! *****\n";
+    return 0;    
 }
