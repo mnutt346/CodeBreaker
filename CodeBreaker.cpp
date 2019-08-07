885f8168f4f6d96e392ca50e3eccc5a2ed0f8e96
Michael Nutt <mnutt346@gmail.com>
2019-08-07 18:25:28 -0500

    Player can play game for one round without any difficulty adjustments.

--- CodeBreaker.cpp
+++ CodeBreaker.cpp
@@ -4,11 +4,16 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main()
+void PrintIntro()
 {
     // Print intro for user.
-    cout << "You have been locked in a room with an electronically-controlled lock..." << endl;
-    cout << "You must enter the correct code to continue..." << endl;
+    cout << "You have been locked in a room with an electronically-controlled lock...\n";
+    cout << "You must enter the correct code to continue...\n\n";
+}
+
+void PlayGame()
+{
+    PrintIntro();
 
     int Num1 = 4;
     int Num2 = 6;
@@ -18,12 +23,31 @@ int main()
     int CodeProduct = Num1 * Num2 * Num3;
 
     // Print CodeSum and CodeProduct for the user.
-    cout << endl; 
     cout << "+ There are 3 numbers in the code." << endl;
     cout << "+ The code adds up to: " << CodeSum << endl;
     cout << "+ The code multiplies to give: " << CodeProduct << endl;
 
-    int PlayerGuess;
+    int Guess1, Guess2, Guess3;
 
+    // Extract PlayerGuess.
+    cin >> Guess1 >> Guess2 >> Guess3;
+
+    int GuessSum = Guess1 + Guess2 + Guess3;
+    int GuessProduct = Guess1 * Guess2 * Guess3;
+
+    // Check if player's guess is correct or not.
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        cout << "You Win!\n" ;
+    }
+    else
+    {
+        cout << "Nope!\n";
+    }
+}
+
+int main()
+{
+    PlayGame();
     return 0;
 }
